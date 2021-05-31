4779a4274108acb9bfe747776d956c95e22f63bb
Kulkarni, Dhruv Rajesh <drkulkar@iu.edu>
2021-05-31 14:19:35 -0400

    Functional App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,246 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState<any>(0);
+  const [first, setFirst] = useState("");
+  const [second, setSecond] = useState("");
+  const [operator, setOperator] = useState("");
+
+  const calculate = () => {
+    switch (operator) {
+      case "+":
+        if (first !== "" && second !== "") {
+          setValue(parseFloat(first) + parseFloat(second));
+        }
+        return;
+      case "-":
+        if (first !== "" && second !== "") {
+          setValue(parseFloat(first) - parseFloat(second));
+        }
+        return;
+      case "x":
+        if (first !== "" && second !== "") {
+          setValue(parseFloat(first) * parseFloat(second));
+        }
+        return;
+      case "/":
+        if (first !== "" && second !== "") {
+          setValue(parseFloat(first) / parseFloat(second));
+        }
+        return;
+    }
+  };
+
+  const createValue = (inner: any, action: any, current: any) => {
+    let temp = current;
+    temp = temp + inner;
+    action(temp);
+    setValue(parseFloat(temp));
+  };
+
+  const backspace = (inner: any, action: any, current: any) => {
+    let temp: string = current;
+    temp = temp.substr(0, temp.length - 1);
+    action(temp);
+    setValue(temp);
+  };
+
+  const PressMeDaddy = (event: any) => {
+    const inner = event.target.innerHTML;
+
+    if (inner === "+" || inner === "-" || inner === "x" || inner === "/") {
+      setOperator(inner);
+      calculate();
+    } else if (inner === "=") {
+      calculate();
+    } else if (inner === "RESET") {
+      setFirst("");
+      setSecond("");
+      setOperator("");
+      setValue(0);
+    } else if (inner === "DEL") {
+      if (first.length >= 1) {
+        backspace(inner, setFirst, first);
+      }
+      if (second.length >= 1) {
+        setSecond(second.substr(0, second.length - 1));
+      }
+    } else {
+      if (first !== "" && operator !== "") {
+        createValue(inner, setSecond, second);
+        console.log(second);
+        return;
+      }
+      if (first.length >= 0) {
+        createValue(inner, setFirst, first);
+        console.log(first);
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Spartan:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <div className="top">
+        <h1>calc</h1>
+        <div className="toggle">toggles</div>
+      </div>
+      <div className="middle">
+        <h1 className="value">{value}</h1>
+      </div>
+      <div className="bottom">
+        <button
+          className="key"
+          onClick={(e: any) => {
+            PressMeDaddy(e);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          1
+        </button>
+        <button
+          className="key"
+          onClick={(e: any) => {
+            PressMeDaddy(e);
+          }}
+        >
+          2
+        </button>
+        <button
+          className="key"
+          onClick={(e: any) => {
+            PressMeDaddy(e);
+          }}
+        >
+          3
+        </button>
+        <button
+          className="key delete"
+          onClick={(e: any) => {
+            PressMeDaddy(e);
+          }}
+        >
+          DEL
+        </button>
+        <button
+          className="key"
+          onClick={(e: any) => {
+            PressMeDaddy(e);
+          }}
+        >
+          4
+        </button>
+        <button
+          className="key"
+          onClick={(e: any) => {
+            PressMeDaddy(e);
+          }}
+        >
+          5
+        </button>
+        <button
+          className="key"
+          onClick={(e: any) => {
+            PressMeDaddy(e);
+          }}
+        >
+          6
+        </button>
+        <button
+          className="key"
+          onClick={(e: any) => {
+            PressMeDaddy(e);
+          }}
+        >
+          +
+        </button>
+        <button
+          className="key"
+          onClick={(e: any) => {
+            PressMeDaddy(e);
+          }}
+        >
+          7
+        </button>
+        <button
+          className="key"
+          onClick={(e: any) => {
+            PressMeDaddy(e);
+          }}
+        >
+          8
+        </button>
+        <button
+          className="key"
+          onClick={(e: any) => {
+            PressMeDaddy(e);
+          }}
+        >
+          9
+        </button>
+        <button
+          className="key"
+          onClick={(e: any) => {
+            PressMeDaddy(e);
+          }}
+        >
+          0
+        </button>
+        <button
+          className="key"
+          onClick={(e: any) => {
+            PressMeDaddy(e);
+          }}
+        >
+          /
+        </button>
+        <button
+          className="key"
+          onClick={(e: any) => {
+            PressMeDaddy(e);
+          }}
+        >
+          .
+        </button>
+        <button
+          className="key"
+          onClick={(e: any) => {
+            PressMeDaddy(e);
+          }}
+        >
+          -
+        </button>
+        <button
+          className="key"
+          onClick={(e: any) => {
+            PressMeDaddy(e);
+          }}
+        >
+          x
+        </button>
+        <button
+          className="key reset"
+          onClick={(e: any) => {
+            PressMeDaddy(e);
+          }}
+        >
+          RESET
+        </button>
+        <button
+          className="key equals"
+          onClick={(e: any) => {
+            PressMeDaddy(e);
+          }}
+        >
+          =
+        </button>
+      </div>
     </div>
   );
 }
